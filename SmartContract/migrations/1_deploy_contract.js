1087c3ef45f5f4840f3d8c4503642cb719a1276a
naisachetti <naisachetti0@gmail.com>
2022-12-09 17:42:30 -0300

    Smart contract structure

---
+++ SmartContract/migrations/1_deploy_contract.js
@@ -0,0 +1,5 @@
+const TheWineGame = artifacts.require("TheWineGame");
+
+module.exports = function(deployer) {
+    deployer.deploy(TheWineGame)
+}
---
+++ SmartContract/test/TestTheWineGame.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// These files are dynamically created at test time
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/TheWineGame.sol";
+
+contract TestTheWineGame {
+    // function testSomething() public {
+
+    // }
+}
